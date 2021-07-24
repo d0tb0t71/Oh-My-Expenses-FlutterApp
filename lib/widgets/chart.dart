41d641af23eda93e3be21f2cf19e09feea8b6d93
Dipon SutraDhar <sdipon.sutradhar@gmail.com>
2021-07-24 18:22:27 +0600

    Version-3

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:oh_my_expenses/models/transaction.dart';
 import 'package:intl/intl.dart';
+import 'package:oh_my_expenses/widgets/chart_bar.dart';
 
 class Chart extends StatefulWidget {
-
   late final List<Transaction> recentTransactions;
 
   Chart(this.recentTransactions);
-
 
   @override
   _ChartState createState() => _ChartState();
 }
 
 class _ChartState extends State<Chart> {
-
-
-
-  List<Map<String, Object>> get groupedTransactionValues {
+  List<Map<String, dynamic>> get groupedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
       var totalSum = 0.0;
@@ -33,23 +29,35 @@ class _ChartState extends State<Chart> {
       print(DateFormat.E().format(weekDay));
       print(totalSum);
 
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
     });
   }
 
-
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     print(groupedTransactionValues);
 
-    return Card(elevation: 6, margin: EdgeInsets.all(20), child: Row(
-      children:
-        groupedTransactionValues.map((data){
-
-          return Text('${data['day']}:${data['amount']}');
-
-        }).toList(),
-    ));
+    return Card(
+        elevation: 6,
+        margin: EdgeInsets.all(20),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactionValues.map((data) {
+              return Flexible(fit: FlexFit.tight, child: ChartBar(data['day'], data['amount'],
+                  totalSpending == 0 ? 0.0 : data['amount'] / totalSpending));
+            }).toList(),
+          ),
+        ));
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:oh_my_expenses/models/transaction.dart';
 import 'package:intl/intl.dart';
+import 'package:oh_my_expenses/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> transactions;
 
-   TransactionList(this.transactions);
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height*0.8,
+      height: MediaQuery.of(context).size.height * 0.6,
       child: transactions.isEmpty
           ? Column(
               children: [
@@ -19,11 +18,11 @@ class TransactionList extends StatelessWidget {
                   height: 30,
                 ),
                 Container(
-                  height: 200,
+                    height: 200,
                     child: Image.asset(
-                  'assets/images/money_full.png',
-                  fit: BoxFit.cover,
-                )),
+                      'assets/images/money_full.png',
+                      fit: BoxFit.cover,
+                    )),
                 Text(
                   'No Expenses',
                   style: TextStyle(fontSize: 30),
@@ -32,50 +31,26 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return Container(
-                  margin: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 2, color: Colors.black45),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Card(
-                      child: Row(
-                    children: [
-                      //Price
-
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.deepOrange),
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2),
-                            borderRadius: BorderRadius.circular(6)),
-                        padding: EdgeInsets.all(10),
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 5,horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: FittedBox(
+                            child: Text('\$${transactions[index].amount}')),
                       ),
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                          color: Colors.deepOrange, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
 
-                      //Titile+Date
 
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transactions[index].title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            style: TextStyle(color: Colors.blueGrey),
-                          )
-                        ],
-                      )
-                    ],
-                  )),
+                  ),
                 );
               },
               itemCount: transactions.length,
